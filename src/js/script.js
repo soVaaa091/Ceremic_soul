38d6bae2488b57f29b93fd22df413f8935079301
Яна Савенкова <savenkovayana50gmail.com>
2026-05-05 18:43:11 +0200

    changed font link to import

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,6 @@ import "purecss/build/grids-responsive-min.css";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-
 import "/src/sass/style.scss";
 
 const burger = document.querySelector(".burger"),
@@ -53,4 +52,24 @@ try {
 
     modules: [Navigation, Pagination],
   });
+} catch (e) {}
+
+try {
+  const tabs = document.querySelectorAll(".catalog__tab");
+  const contents = document.querySelectorAll(".catalog__content-item");
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      // Удаляем активный класс у всех табов и контента
+      tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
+      contents.forEach((c) => (c.style.display = "none"));
+
+      // Добавляем активный класс к нажатому табу и показываем соответствующий контент
+      tab.classList.add("catalog__tab_active");
+      contents[index].style.display = "block";
+    });
+  });
+
+  // Показываем первый контент при загрузке
+  contents.forEach((c, i) => (c.style.display = i === 0 ? "block" : "none"));
 } catch (e) {}
